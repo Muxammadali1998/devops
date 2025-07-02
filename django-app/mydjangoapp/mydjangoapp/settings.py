9ca6d6bc41a2bae28cbc937485da29c4116fd35a
Muxammadali1998 <turgunaliyev.98@gmail.com>
2025-07-02 11:03:27 +0500

    fix static files

--- django-app/mydjangoapp/mydjangoapp/settings.py
+++ django-app/mydjangoapp/mydjangoapp/settings.py
@@ -50,8 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'mydjangoapp.urls'
 
 TEMPLATES = [
@@ -121,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
