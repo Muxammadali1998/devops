bc1f1855d2cfdfa67092dbf16100bbe9b56c6952
Muxammadali1998 <turgunaliyev.98@gmail.com>
2025-07-02 10:45:12 +0500

    change db to PostgreSQL

--- django-app/mydjangoapp/mydjangoapp/settings.py
+++ django-app/mydjangoapp/mydjangoapp/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'mydjangoapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'db_django',
+           'USER': 'test',
+           'PASSWORD': '1234',
+           'HOST': 'localhost',
+           'PORT': '5432',
+       }
 }
 
 
